c909bec20853bcb42fd03a395aec79d270460eca
limberbrevton0-ship-it <limberbrevton0@gmail.com>
2025-11-01 11:32:11 +0700

    tambah cuaca

--- api_client.py
+++ api_client.py
@@ -1,19 +1,26 @@
 from PyQt6.QtNetwork import QNetworkAccessManager,QNetworkRequest, QNetworkReply
-from PyQt6.QtCore import QUrl,QObject
+from PyQt6.QtCore import QUrl,QObject, pyqtSignal
 import json
 class Apiclient(QObject):
+    signal_finished=pyqtSignal(object)
     def __init__(self):
         super().__init__()
         self.manager=QNetworkAccessManager(self)
         self.manager.finished.connect(self.on_finished)
     def on_finished(self,reply: QNetworkReply):
         err=reply.error()
+        print(err)
         if err == QNetworkReply.NetworkError.NoError:
             data=reply.readAll().data()
             print("Data recevied")
+            self.signal_finished.emit(data)
         else:print("Error:",reply.errorString())
     def post(self,url,data):
         request_url=QNetworkRequest(QUrl(url))
         request_url.setHeader(QNetworkRequest.KnownHeaders.ContentTypeHeader,"application/json")
         data_json=json.dumps(data).encode("utf-8")
         self.manager.post(request_url,data_json)
+    def get(self,url):
+        request_url=QNetworkRequest(QUrl(url))
+        self.manager.get(request_url)
+        print("get data from",url)
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from PyQt6.QtWidgets import  QMainWindow, QApplication
 from PyQt6.uic import loadUi
 import sys
+from cuaca.Weather import Weather
+from telur.TelurWidget import TelurWidget
 class MainWindow(QMainWindow):
    def __init__(self):
         super().__init__()
         loadUi("main.ui",self)
+        self.listWidget.itemDoubleClicked.connect(self.on_item_double_clicked)
+   def on_item_double_clicked(self,item):
+        if item.text() == "telur":
+            self.telur_window = TelurWidget()
+            self.telur_window.show()
+        elif item.text() == "cuaca":
+            self.cuaca_window = Weather()
+            self.cuaca_window.show()
 
 app = QApplication(sys.argv)
 
