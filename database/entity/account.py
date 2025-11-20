cc907c0f62e5c504cbc2b60dcb6b4e790d05f2a4
walden-1 <yukehandsome@gmail.com>
2025-11-20 18:13:05 +0700

    commit

--- database/entity/account.py
+++ database/entity/account.py
@@ -1,10 +1,9 @@
+from sympy import use
 from .base import Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
 class Account(Base):
     __tablename__ = 'accounts'
-    
-    id=Column(Integer,primary_key=True)
-    site=Column(String,nullable=False)
-    username=Column(String,nullable=False)
-    password=Column(String,nullable=False)
+    id = Column(Integer, primary_key=True)
+    site = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
